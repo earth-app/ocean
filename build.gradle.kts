6c7b3a8e150b1c405e38ac3bdccabdaa2e227da0
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-05-03 20:50:29 -0500

    whoops

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ plugins {
 }
 
 val v = "1.0.0"
+
+group = "com.earth-app.ocean"
 version = "${if (project.hasProperty("snapshot")) "$v-SNAPSHOT" else v}${project.findProperty("suffix")?.toString()?.run { "-${this}" } ?: ""}"
 val desc = "Recommender Algorithm for The Earth App"
 description = desc
