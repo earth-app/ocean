accc123b046b2fbd0e0f1a7a005d131e1d245df4
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-05-24 04:54:28 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,6 @@ kotlin {
     }
 
     sourceSets {
-        val cryptoVersion = "0.4.0"
-
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
