939378b92a46c468f74f130f8c290239962b7e56
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-06 02:23:19 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation("com.earth-app.shovel:shovel:1.0.1")
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
             implementation("io.github.oshai:kotlin-logging:7.0.7")
