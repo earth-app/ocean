6f84460e3f923d716066fbe89e548ce47a152a53
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-10 03:48:55 -0500

    feat: add string compression serializer

--- src/common/main/com/earthapp/serializers.kt
+++ src/common/main/com/earthapp/serializers.kt
@@ -3,6 +3,18 @@ package com.earthapp
 import com.earthapp.account.Account
 import com.earthapp.activity.Activity
 import com.earthapp.event.Event
+import com.earthapp.ocean.boat.Scraper
+import korlibs.io.compression.compress
+import korlibs.io.compression.deflate.GZIP
+import korlibs.io.compression.uncompress
+import korlibs.io.lang.Charsets
+import korlibs.io.lang.decodeToString
+import korlibs.io.lang.encodeToByteArray
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
@@ -13,6 +25,7 @@ internal val serializers = SerializersModule {
         subclass(Account.serializer())
         subclass(Event.serializer())
         subclass(Activity.serializer())
+        subclass(Scraper.Page.serializer())
     }
 }
 
@@ -21,4 +34,21 @@ internal val json = Json {
     isLenient = true
     prettyPrint = true
     serializersModule = serializers
+}
+
+object CompressionSerializer : KSerializer<String> {
+    private val charset = Charsets.UTF8
+
+    override val descriptor = PrimitiveSerialDescriptor("CompressedString", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: String) {
+        val bytes = value.encodeToByteArray(charset).compress(GZIP)
+        encoder.encodeString(bytes.decodeToString(charset))
+    }
+
+    override fun deserialize(decoder: Decoder): String {
+        val compressed = decoder.decodeString()
+        val bytes = compressed.encodeToByteArray(charset).uncompress(GZIP)
+        return bytes.decodeToString(charset)
+    }
 }
