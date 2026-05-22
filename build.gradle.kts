e7bee8736d7e43f39e4deaec4a3af742e68c1467
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-05-22 18:31:07 -0500

    chore: bump version to v1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     signing
 }
 
-val v = "1.0.4"
+val v = "1.1.0"
 
 group = "com.earth-app.ocean"
 version = "${if (project.hasProperty("snapshot")) "$v-SNAPSHOT" else v}${project.findProperty("suffix")?.toString()?.run { "-${this}" } ?: ""}"
